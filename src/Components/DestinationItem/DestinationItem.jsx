7d822f107463957cf1af9cd8e155ada8f9d19525
Narise Nanda <nagababunarise>
2024-08-21 11:54:02 +0530

    Destination

---
+++ src/Components/DestinationItem/DestinationItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./DestinationItem.css";
+
+const DestinationItem = (props) => {
+  const { destinationDetails } = props;
+  const { imgUrl, name } = destinationDetails;
+  return (
+    <li className="destination-item">
+      <img src={imgUrl} alt={name} className="destination-image" />
+      <p className="name">{name}</p>
+    </li>
+  );
+};
+
+export default DestinationItem;
